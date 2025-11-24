6a18eaa29922dde05481e1c70346f72adcd2bd7a
pi-man <pjoyce1607@gmail.com>
2025-11-23 20:07:18 -0700

    register page title

--- MumeiBot/register.cpp
+++ MumeiBot/register.cpp
@@ -75,7 +75,7 @@ int register_callback(struct mg_connection * conn, void * cbdata) {
 			response_headers["Connection"] = "close";
 			mg_printf(conn,
 				"HTTP/1.1 200 OK\r\n%s", std::string(response_headers).c_str());
-			mg_printf(conn, "<!DOCTYPE html><html><body>");
+			mg_printf(conn, "<!DOCTYPE html><title>Register New Link</title><html><body>");
 			mg_printf(conn, "<p>Welcome %s!</p>", name.c_str());
 			if (json.is<picojson::array>()) {
 				picojson::array jobj = json.get<picojson::array>();
