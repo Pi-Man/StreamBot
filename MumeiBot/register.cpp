de09fd1884fe8ae9b8d6b2a5e042b108e99a758f
pi-man <pjoyce1607@gmail.com>
2025-11-25 19:50:48 -0700

    clean debug prints

--- MumeiBot/register.cpp
+++ MumeiBot/register.cpp
@@ -178,7 +178,7 @@ int oauth_callback(struct mg_connection * conn, void * cbdata) {
 				uuid = uuid_generator.getUUID().str();
 				usermap[uuid] = { access_token, refresh_token };
 				r_usermap[access_token] = uuid.c_str();
-				printf("created new user %s", uuid.c_str());
+				printf("created new user %s\n", uuid.c_str());
 			}
 				
 			usermap_mutex.unlock();
